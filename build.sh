d4f90977fdc9336487f315e0bf2845c648e0289d
Nomekuma <t.gunness35@gmail.com>
2023-10-03 14:20:00 +0400

    test

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-# !usr/bin/env bash
-# exit on error
-set -o errexit
-pip install -r requirements.txt
-cd myproject
+# # !usr/bin/env bash
+# # exit on error
+# set -o errexit
+# pip install -r requirements.txt
+# cd myproject
 
-python manage.py makemigrations
-python manage.py migrate
-# python manage.py collectstatic --noinput
+# python manage.py makemigrations
+# python manage.py migrate
+# # python manage.py collectstatic --noinput
